2ce83806fc566db58fb2c92997757696a0bad4e3
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-08-12 19:43:54 -0300

    Add navbar and colors

--- src/home.js
+++ src/home.js
@@ -10,10 +10,6 @@ function createSections() {
 
 function addBgToSections() {
   const sections = document.getElementsByClassName('main-sections');
-  console.log(sections.length);
-  /* for (let i = 0; i < sections.length; i += 1) {
-
-  } */
 }
 
 export { createSections, addBgToSections };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import './style.scss';
 import { createSections, addBgToSections } from './home';
 
+function createNavbar() {
+  const body = document.getElementsByTagName('body')[0];
+  const header = document.createElement('header');
+  const navbar = document.createElement('nav');
+  body.insertBefore(header, body.childNodes[0]);
+  header.appendChild(navbar);
+  return navbar;
+}
+
+function addListToNavbar(navbar, names) {
+  const list = document.createElement('ul');
+  for (let i = 0; i < 4; i += 1) {
+    const aTag = document.createElement('a');
+    aTag.href = `#${names[i]}`.toLowerCase();
+    list.appendChild(aTag);
+  }
+  navbar.appendChild(list);
+  return list;
+}
+
+function addNamesToList(names, list) {
+  const listLength = list.children.length;
+  for (let i = 0; i < listLength; i += 1) {
+    const listElement = document.createElement('li');
+    listElement.setAttribute('id', `nav-li${i}`);
+    listElement.innerHTML = `${names[i]}`;
+    list.children[i].appendChild(listElement);
+  }
+}
+
+const listNames = ['HOME', 'MENU', 'ABOUT US', 'CONTACT'];
+
+
 createSections();
 addBgToSections();
+const list = addListToNavbar(createNavbar(), listNames);
+addNamesToList(listNames, list);
