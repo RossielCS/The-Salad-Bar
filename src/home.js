9f9c607366f73f00fed44f48d824736ee8cdcdea
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-08-14 18:50:24 -0300

    Add content to article
    - Css styling for home

--- src/home.js
+++ src/home.js
@@ -1,57 +1,134 @@
-function createBgDivs(content) {
-  const bgDivContainer = document.createElement('div');
+/*
+const ElementFactory = (newElement, parent, typeAtr, nameAtr) => {
+  const getName = () => name;
+  const getPiece = () => piece;
+  const getScore = () => score;
+  const createElement = () => {
+    const child = document.createElement(`${newElement}`);
+    parent.appendChild(child);
+    return child;
+  };
+  const addAtribute = () => {
+    if (typeAtr === 'class') {
+      createElement.setAttribute('class', `${nameAtr}`);
+    }
+  };
+  return {
+    getName,
+    getPiece,
+    getScore,
+    updateScore() {
+      score += 1;
+    },
+  };
+}; */
+
+export function creator(parent, newElement, position) {
+  const child = document.createElement(`${newElement}`);
+  if (position === 'append') {
+    parent.appendChild(child);
+  } else {
+    parent.insertBefore(child, position);
+  }
+  return child;
+}
+
+export function addListToNavbar(navbar, tabNames) {
+  const tempNames = [...tabNames];
+  for (let i = 0; i < 2; i += 1) {
+    const list = creator(navbar, 'ul', 'append');
+    for (let j = 0; j < 2; j += 1) {
+      const aTag = creator(list, 'a', 'append');
+      aTag.href = `#${tempNames[j]}`.toLowerCase();
+    }
+    tempNames.splice(0, 2);
+  }
+  return navbar.children;
+}
+
+export function addNamesToList(tabNames, list) {
+  const tempNames = [...tabNames];
+  for (let i = 0; i < list.length; i += 1) {
+    for (let j = 0; j < 2; j += 1) {
+      const listElement = creator(list[i].children[j], 'li', 'append');
+      listElement.setAttribute('id', `nav-li${j}`);
+      listElement.innerHTML = `${tempNames[j]}`;
+    }
+    tempNames.splice(0, 2);
+  }
+}
+
+export function addLogoToNav(navbar) {
+  const logo = creator(navbar, 'div', navbar.childNodes[1]);
+  logo.setAttribute('id', 'home-logo');
+  return logo;
+}
+
+/* export function addPointsBetweenLi(navbar) {
+  const point1 = creator(navbar, 'div', navbar.childNodes[0].childNodes[1]);
+  point1.setAttribute('class', 'points-li');
+} */
+
+export function createBgDivs(content) {
+  const bgDivContainer = creator(content, 'div', content.childNodes[0]);
   bgDivContainer.setAttribute('class', 'container-home-bg');
   for (let i = 0; i < 7; i += 1) {
-    const bgDiv = document.createElement('div');
+    const bgDiv = creator(bgDivContainer, 'div', 'append');
     bgDiv.setAttribute('id', `home-bg${i}`);
-    bgDivContainer.appendChild(bgDiv);
   }
-  content.insertBefore(bgDivContainer, content.childNodes[0]);
   return bgDivContainer;
 }
 
-function createArticles(main) {
+export function createArticles(main) {
   for (let i = 0; i < 7; i += 1) {
-    const article = document.createElement('article');
+    const article = creator(main, 'article', 'append');
     article.setAttribute('id', `home-art${i}`);
-    main.appendChild(article);
   }
   return main.children;
 }
 
-function addSectionsToArticles() {
-  const art0 = document.getElementById('home-art0');
-  const art1 = document.getElementById('home-art1');
-  const art3 = document.getElementById('home-art3');
-  const art6 = document.getElementById('home-art6');
-  const arr = [art0, art1, art3, art6];
+export function addSectionsToArticles() {
+  const arr = [
+    document.getElementById('home-art0'),
+    document.getElementById('home-art1'),
+    document.getElementById('home-art3'),
+    document.getElementById('home-art6'),
+  ];
   for (let i = 0; i < arr.length; i += 1) {
-    const section1 = document.createElement('div');
-    section1.setAttribute('class', 'home-art-sect');
-    const section2 = document.createElement('div');
-    section2.setAttribute('class', 'home-art-sect');
-    arr[i].appendChild(section1);
-    arr[i].appendChild(section2);
+    for (let j = 0; j < 2; j += 1) {
+      const section = creator(arr[i], 'div', 'append');
+      section.setAttribute('class', 'home-art-sect');
+    }
   }
 }
 
-function addContentToArt0() {
+export function addContentToArt0() {
   const art0 = document.getElementById('home-art0').children[1];
-  const title = document.createElement('h1');
+  const title = creator(art0, 'h1', 'append');
   title.innerHTML = 'Pellentesque dignissim enim sit amet venenatis urna cursus eget.';
-  const pElement = document.createElement('p');
-  const text = document.createTextNode('Sed egestas egestas fringilla phasellus faucibus. Sed pulvinar proin gravida hendrerit lectus a. Iaculis urna id volutpat lacus laoreet non. Nunc eget lorem dolor sed viverra ipsum nunc.');
-  pElement.appendChild(text);
-  const button = document.createElement('button');
+
+  const pElement = creator(art0, 'p', 'append');
+  pElement.innerHTML = 'Sed egestas egestas fringilla phasellus faucibus. Sed pulvinar proin gravida hendrerit lectus a. Iaculis urna id volutpat lacus laoreet non. Nunc eget lorem dolor sed viverra ipsum nunc.';
+
+  const button = creator(art0, 'button', 'append');
   button.innerHTML = 'BUY SALAD';
   button.addEventListener('click', () => {
-
   });
-  art0.appendChild(title);
-  art0.appendChild(pElement);
-  art0.appendChild(button);
 }
 
-export {
-  createBgDivs, createArticles, addSectionsToArticles, addContentToArt0,
-};
+export function addContentToArt1() {
+  const art1Child0 = document.getElementById('home-art1').children[0];
+  const art1Child1 = document.getElementById('home-art1').children[1];
+
+  const title0 = creator(art1Child0, 'h2', 'append');
+  title0.innerHTML = 'Pellentesque dignissim enim sit amet venenatis urna cursus eget.';
+
+  const pElement0 = creator(art1Child0, 'p', 'append');
+  pElement0.innerHTML = 'Sed egestas egestas fringilla phasellus faucibus. Sed pulvinar proin gravida hendrerit lectus a. Iaculis urna id volutpat lacus laoreet non. Nunc eget lorem dolor sed viverra ipsum nunc.';
+
+  const title1 = creator(art1Child1, 'h2', 'append');
+  title1.innerHTML = 'Pellentesque dignissim enim sit amet venenatis urna cursus eget.';
+
+  const pElement1 = creator(art1Child1, 'p', 'append');
+  pElement1.innerHTML = 'Sed egestas egestas fringilla phasellus faucibus. Sed pulvinar proin gravida hendrerit lectus a. Iaculis urna id volutpat lacus laoreet non. Nunc eget lorem dolor sed viverra ipsum nunc.';
+}
--- src/index.js
+++ src/index.js
@@ -2,91 +2,21 @@ import './style.scss';
 import './assets/images/home_art0.png';
 import './assets/images/home_bg02.jpg';
 import './assets/images/home_nav_logo.png';
-import {
-  createBgDivs, createArticles, addSectionsToArticles, addContentToArt0,
-} from './home';
-
-function createHeader(content) {
-  const header = document.createElement('header');
-  content.appendChild(header);
-  return header;
-}
-
-function createNavbar(header) {
-  const navbar = document.createElement('nav');
-  header.appendChild(navbar);
-  return navbar;
-}
-
-function addListToNavbar(navbar, tabNames) {
-  const tempNames = [...tabNames];
-  for (let i = 0; i < 2; i += 1) {
-    const list = document.createElement('ul');
-    for (let j = 0; j < 2; j += 1) {
-      const aTag = document.createElement('a');
-      aTag.href = `#${tempNames[j]}`.toLowerCase();
-      list.appendChild(aTag);
-    }
-    tempNames.splice(0, 2);
-    navbar.appendChild(list);
-  }
-  return navbar.children;
-}
-
-function addNamesToList(tabNames, list) {
-  const tempNames = [...tabNames];
-  for (let i = 0; i < list.length; i += 1) {
-    for (let j = 0; j < 2; j += 1) {
-      const listElement = document.createElement('li');
-      listElement.setAttribute('id', `nav-li${j}`);
-      listElement.innerHTML = `${tempNames[j]}`;
-      list[i].children[j].appendChild(listElement);
-    }
-    tempNames.splice(0, 2);
-  }
-}
-
-function addLogoToNav(navbar) {
-  const logo = document.createElement('div');
-  logo.setAttribute('id', 'home-logo');
-  navbar.insertBefore(logo, navbar.childNodes[1]);
-  return logo;
-}
-
-function addPointsBetweenLi(navbar) {
-  const point1 = document.createElement('div');
-  point1.setAttribute('class', 'points-li');
-  const point2 = document.createElement('div');
-  point2.setAttribute('class', 'points-li');
-
-  navbar.children[0].insertBefore(point1, navbar.children[0].children[1]);
-  navbar.children[2].insertBefore(point2, navbar.children[2].children[1]);
-}
-
-function createMain(content) {
-  const main = document.createElement('main');
-  content.appendChild(main);
-  return main;
-}
-
-function createFooter(content) {
-  const footer = document.createElement('footer');
-  content.appendChild(footer);
-  return footer;
-}
+import * as homeFunctions from './home';
 
 const content = document.getElementById('content');
 const tabNames = ['HOME', 'MENU', 'ABOUT US', 'CONTACT'];
-const header = createHeader(content);
-const navbar = createNavbar(header);
-const list = addListToNavbar(navbar, tabNames);
+const header = homeFunctions.creator(content, 'header');
+const navbar = homeFunctions.creator(header, 'nav');
+const list = homeFunctions.addListToNavbar(navbar, tabNames);
 
-addNamesToList(tabNames, list);
-addLogoToNav(navbar);
-const main = createMain(content);
-createFooter(content);
-createBgDivs(content);
-createArticles(main);
-addSectionsToArticles();
-addContentToArt0();
-addPointsBetweenLi(navbar);
+homeFunctions.addNamesToList(tabNames, list);
+homeFunctions.addLogoToNav(navbar);
+const main = homeFunctions.creator(content, 'main');
+homeFunctions.creator(content, 'footer');
+homeFunctions.createBgDivs(content);
+homeFunctions.createArticles(main);
+homeFunctions.addSectionsToArticles();
+homeFunctions.addContentToArt0();
+// homeFunctions.addPointsBetweenLi(navbar);
+homeFunctions.addContentToArt1();
