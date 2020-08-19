821890351212957aa9f163514442c9d1eb89c8d8
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-08-19 14:10:18 -0300

    Add event listeners to deals list
    - Remove unnecessary images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './assets/stylesheets/style.scss';
 import './assets/images/home_nav_logo.png';
+import './assets/images/deals_00.png';
 import { tabNames, createHeader, creator } from './header';
 import { createHome } from './home';
 import createAboutUs from './about-us';
@@ -15,18 +16,22 @@ const home = createHome(main, 'home', 2);
 const menu = document.getElementsByTagName('ul')[0];
 let currentPage = home;
 
+// Logo
 document.getElementById('home-logo').addEventListener('click', () => {
   main.removeChild(currentPage);
   const home = createHome(main, 'home', 2);
   currentPage = home;
 });
 
+// Promotions
 menu.children[1].addEventListener('click', () => {
   main.removeChild(currentPage);
   const promotions = createPromotions(main, 2, dealsInfo);
   currentPage = promotions;
+  document.getElementById('promo-menu0').click();
 });
 
+// About Us
 menu.children[3].addEventListener('click', () => {
   main.removeChild(currentPage);
   const aboutUs = createAboutUs(main, 1);
--- src/promotions.js
+++ src/promotions.js
@@ -8,11 +8,18 @@ const dealsInfo = {
   '04': 'Set Your Kitchen, Save Your Body',
 };
 
+const dealsBgImages = {
+  'promo-menu0': 'deals_00.png',
+  'promo-menu1': 'deals_01.png',
+  'promo-menu2': 'deals_02.png',
+  'promo-menu3': 'deals_03.png',
+};
+
 function dealsList(section, dealsInfo) {
   const menuList = creator(section, 'ul', 'append');
   for (let i = 0; i < 4; i += 1) {
     const li = creator(menuList, 'li', 'append');
-    li.setAttribute('id', `deals-menu${i}`);
+    li.setAttribute('id', `promo-menu${i}`);
     const number = creator(li, 'div', 'append');
     number.innerHTML = Object.keys(dealsInfo)[i];
     const info = creator(li, 'p', 'append');
@@ -30,10 +37,22 @@ function addContentToSections(sections, dealsInfo) {
   dealsList(sect1, dealsInfo);
 }
 
-function createPromotions(main, numSections, dealsInfo) {
+function addEventToLi(dealsBgImages) {
+  const liContainer = document.getElementsByTagName('ul')[1].childNodes;
+  liContainer.forEach(x => {
+    x.addEventListener('click', () => {
+      const section = document.getElementsByClassName('art-promotions-sect')[0];
+      console.log(x);
+      section.style.backgroundImage = "url('../images/deals_00.png')";
+    });
+  });
+}
+
+function createPromotions(main, numSections, dealsInfo, dealsBgImages) {
   const article = createArticle(main, 'promotions');
   const sections = addSectionsToArticle(article, numSections);
   addContentToSections(sections, dealsInfo);
+  addEventToLi(dealsBgImages);
   return article;
 }
 
